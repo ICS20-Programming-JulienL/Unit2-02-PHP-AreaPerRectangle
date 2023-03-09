0855c75ed8f6808f69bfece102e0e264d077d3b7
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-03-09 01:04:54 +0000

    Finished website.

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+    <!--Meta Data-->
     <meta charset="utf-8">
     <meta name="description" content="My first webpage, with JavaScript">
     <meta name="keywords" content="immaculata, ics2o">
     <meta name="author" content="Julien Lamoureux">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/favicon-16x16.png">
-    <link rel="manifest" href="./fav_index/site.webmanifest">
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index1/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index1/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index1/favicon-16x16.png">
+    <link rel="manifest" href="./fav_index1/site.webmanifest">
     <link rel="stylesheet" href="./css/style.css">
     <title>Area & Perimeter of Rectangle in PHP</title>
 	</head>
 	<body>
+    <!--Header and Text-->
     <h1><?php echo 'Area & Perimeter of a Rectangle'; ?></h1>
+    <!--Variables-->
 		<?php 
 			$length = 6;  
 		 	$width = 12; 
